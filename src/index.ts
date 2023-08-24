3c8d8ca16468457eb077c2bf197f99457b177b66
fregnani123 <fabianofregnani@hotmail.com>
2023-08-23 21:26:20 -0300

    estudos

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 
 const app = express();
 
-let produtos: number;
 
- 
-app.get('/produtos', (req, res) => {
-    res.send('Meus Produtos')
-}).listen(3000, () => {
-    console.log('Servidor rodando na PORT:3000');
+interface Iproduct{
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    address: object;
+
+}
+
+const produts: Iproduct[] = []
+
+
+app.get('/produtos', (req: Request, res: Response) => {
+    const newProduct = {
+        id: Math.random(),
+        name: "Batedeira",
+        price: 300,
+        description: 'Batedeira potente, boa para fazer bolos',
+        address: {
+            street: 'Rua dos Bobos',
+            number: 12,
+            state: "SP"
+        }
+    }
+    
+    produts.push(newProduct)   
+
+  res.json(produts)
+
+})
+    
+app.listen(3000, () => {
+console.log('Servidor rodando na PORT:3000');
 });
 
